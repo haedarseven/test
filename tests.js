4ed5e8b58e5a8e4b35e30e8c4d915c27568d4904
haedarseven <85310108+haedarseven@users.noreply.github.com>
2023-06-14 16:44:59 +0300

    Create tests.js

---
+++ tests.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>The iframe element</h1>
+
+<iframe
+    src="https://ipfs-2.thirdwebcdn.com/ipfs/QmUe6MdTHr4it2k7rMkLZZ44TGWVKZSFEcYCbrCzuD7LP4/erc20.html?contract=0x1639921BD8Ebb88736E05780ADf55bE47D79208A&chain=%7B%22name%22%3A%22Linea+Testnet%22%2C%22chain%22%3A%22ETH%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Flinea-testnet.rpc.thirdweb.com%2F5a9bc94b87f7cbbbfbbc234bf1e07f0adf5f3cf3012c9f26f9fc9820d64df93a%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Linea+Ether%22%2C%22symbol%22%3A%22ETH%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22linea-testnet%22%2C%22chainId%22%3A59140%2C%22testnet%22%3Atrue%2C%22slug%22%3A%22linea-testnet%22%7D&primaryColor=purple"
+    width="600px"
+    height="600px"
+    style="max-width:100%;"
+    frameborder="0"
+    ></iframe>
+</iframe>
+
+</body>
+</html>
